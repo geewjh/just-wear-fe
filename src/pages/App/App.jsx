1b70f445b2ad66de415cc33c029b688f3664a30c
geewjh <geewjh@gmail.com>
2024-06-05 03:06:24 +0800

    feat: add route and test route from home pg

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import AuthPage from "../AuthPage/AuthPage";
 import HomePage from "../HomePage/HomePage";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(true);
 
   return (
     <>
       <main className="bg-neutral-content text-4xl font-bold">
         {user ? (
           <Routes>
-            <HomePage />
+            <Route path="/home" element={<HomePage />} />
           </Routes>
         ) : (
           <AuthPage />
