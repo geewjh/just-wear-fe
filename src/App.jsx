84ee9ed26da5867b45f47c5a860cf8502bd87072
geewjh <geewjh@gmail.com>
2024-06-04 09:09:38 +0800

    feat: install daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold">You Got This!</h1>
+      <h1 className="bg-neutral-content text-4xl font-bold">You Got This!</h1>
     </>
   );
 }
