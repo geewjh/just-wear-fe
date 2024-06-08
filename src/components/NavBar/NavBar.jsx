3a0944cd54b47ffff24458c85399748f7ece284d
geewjh <geewjh@gmail.com>
2024-06-08 11:41:58 +0800

    fix: typo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
-import { logOutService } from "../../utilitities/users-service";
+import { logOutService } from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
