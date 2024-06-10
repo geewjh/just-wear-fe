09d0bcbab30a2892238026f3b38d403ee3c61b34
geewjh <geewjh@gmail.com>
2024-06-10 16:09:19 +0800

    feat: add link for closet and favourites

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
+import { BiCloset } from "react-icons/bi";
+import { FaRegStar } from "react-icons/fa";
 import { logOutService } from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
@@ -11,6 +13,7 @@ export default function NavBar({ user, setUser }) {
     setUser(null);
     navigate("/");
   };
+
   return (
     <nav className=" p-4 flex justify-between items-center">
       <div className="flex items-center space-x-4">
@@ -19,6 +22,18 @@ export default function NavBar({ user, setUser }) {
           className="text-white text-sm hover:text-zinc-400 transition duration-150 ease-in-out"
         >
           <IoHomeOutline className="text-2xl" />
+        </Link>
+        <Link
+          to="/closet"
+          className="text-white text-sm hover:text-zinc-400 transition duration-150 ease-in-out"
+        >
+          <BiCloset className="text-2xl" />
+        </Link>
+        <Link
+          to="/closet/favourites"
+          className="text-white text-sm hover:text-zinc-400 transition duration-150 ease-in-out"
+        >
+          <FaRegStar className="text-2xl" />
         </Link>
       </div>
       <h3 className="text-xl text-zinc-400">{user.username}&#39;s Closet</h3>
