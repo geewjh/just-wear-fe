71eea3b41db6ab9524049879ab18065b3b6cf3f3
geewjh <geewjh@gmail.com>
2024-06-10 12:20:40 +0800

    feat: add useLocation to auth pg

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,18 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import { useLocation } from "react-router-dom";
 
 export default function AuthPage({ setUser }) {
+  const location = useLocation();
+
   return (
     <div>
       <main className="container flex mx-auto h-[90vh] items-center justify-center">
-        <SignUpForm setUser={setUser} />
-        <LoginForm setUser={setUser} />
+        {location.pathname === "/register" ? (
+          <SignUpForm setUser={setUser} />
+        ) : location.pathname === "/login" ? (
+          <LoginForm setUser={setUser} />
+        ) : null}
       </main>
     </div>
   );
