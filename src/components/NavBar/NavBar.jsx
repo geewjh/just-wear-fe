a16ba6c86827e83b4b805b42dd94b22751c9ea86
geewjh <geewjh@gmail.com>
2024-06-10 15:07:09 +0800

    fix: handleLogOut refreshing issue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
 import { logOutService } from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
-  function handleLogOut() {
+  const navigate = useNavigate();
+
+  const handleLogOut = (e) => {
+    e.preventDefault();
     logOutService();
     setUser(null);
-  }
+    navigate("/");
+  };
 
   return (
     <nav>
