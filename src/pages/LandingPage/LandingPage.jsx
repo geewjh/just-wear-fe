1d444aefe5c315eb7237b636044a78b0e6b4fb16
geewjh <geewjh@gmail.com>
2024-06-10 16:26:08 +0800

    style: style landing pg

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,11 @@
-import background from "../../assets/images/landing-pic.jpg";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
     <div className="h-screen w-full relative">
-      <img
-        src={background}
-        className="absolute inset-0 w-full h-full object-cover opacity-80"
-        alt="Landing background"
-      />
       <div className="absolute inset-0 flex items-center justify-center p-12">
         <div className="text-center">
-          <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          <h2 className="text-4xl font-bold text-white ">
             Welcome to Just Wear 👕
           </h2>
           <p className="mt-4 text-white/90 leading-relaxed">
