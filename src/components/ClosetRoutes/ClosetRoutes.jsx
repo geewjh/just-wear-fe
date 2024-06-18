6a3c6a9b9970adcecd61507c120d8242e719bcb1
geewjh <geewjh@gmail.com>
2024-06-18 19:17:37 +0800

    fix: remove favourite page

--- src/components/ClosetRoutes/ClosetRoutes.jsx
+++ src/components/ClosetRoutes/ClosetRoutes.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import ClothesForm from "../ClothesForm/ClothesForm";
 import ClosetPage from "../../pages/ClosetPage/ClosetPage";
-import FavouritesPage from "../../pages/FavouritesPage/FavouritesPage";
 
 export default function ClosetRoutes() {
   return (
     <Routes>
       <Route index element={<ClosetPage />} />
       <Route path="/clothes/new" element={<ClothesForm />} />
-      <Route path="/favourites" element={<FavouritesPage />} />
     </Routes>
   );
 }
--- src/pages/FavouritesPage/FavouritesPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function FavouritesPage() {
-  return (
-    <h1>
-      <div>This is where your favourites clothes are at!</div>
-    </h1>
-  );
-}
